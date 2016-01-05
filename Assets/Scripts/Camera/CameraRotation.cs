385da7770e616184180c9ab072cba86a4accf430
Sam <TheSkeltch@gmail.com>
2016-01-05 03:26:01 -0500

    START

---
+++ Assets/Scripts/Camera/CameraRotation.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraRotation : MonoBehaviour
+{
+    public float horizontalSpeed = 2.0F;
+    public float verticalSpeed = 2.0F;
+    void Update()
+    {
+        float h = horizontalSpeed * Input.GetAxis("Mouse X");
+        float v = verticalSpeed * Input.GetAxis("Mouse Y");
+        transform.Rotate(v, h, 0);
+    }
+}
---
+++ Assets/Scripts/Camera/CameraTouch.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraTouch : MonoBehaviour
+{
+
+    Vector3 firstPoint;
+    Vector3 secondPoint;
+    float xAngTemp = 0.0f;
+    float yAngTemp = 0.0f;
+    float xAngle = 0.0f;
+    float yAngle = 0.0f;
+    // Use this for initialization
+    void Start()
+    {
+        this.transform.rotation = Quaternion.Euler(yAngle, xAngle, 0.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            firstPoint = Input.GetTouch(0).position;
+            xAngTemp = xAngle;
+            yAngTemp = yAngle;
+        }
+        if (Input.GetTouch(0).phase == TouchPhase.Moved)
+        {
+            secondPoint = Input.GetTouch(0).position;
+            xAngle = xAngTemp + (secondPoint.x - firstPoint.x) * 180.0f / Screen.width;
+            yAngle = yAngTemp - (secondPoint.y - firstPoint.y) * 90.0f / Screen.height;
+            this.transform.rotation = Quaternion.Euler(yAngle, xAngle, 0.0f);
+        }
+    }
+}
